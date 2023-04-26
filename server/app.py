293de988f303f7d36a58e5bad08b71bc84a0cc28
amanda ranero <amandaranero@gmail.com>
2023-04-26 12:36:45 -0400

    done

--- server/app.py
+++ server/app.py
@@ -30,17 +30,54 @@ def bakeries():
     )
     return response
 
-@app.route('/bakeries/<int:id>')
+@app.route('/bakeries/<int:id>', methods =['GET', 'PATCH'])
 def bakery_by_id(id):
 
     bakery = Bakery.query.filter_by(id=id).first()
-    bakery_serialized = bakery.to_dict()
+
+    if request.method == 'GET':
+        bakery_serialized = bakery.to_dict()
+
+        response = make_response(
+            bakery_serialized,
+            200
+        )
+    elif request.method == 'PATCH':
+        for key in request.form:
+            setattr(bakery, key, request.form.get(key))
+
+        db.session.add(bakery)
+        db.session.commit()
+
+        bakery_serial = bakery.to_dict()
+
+        response = make_response(
+            bakery_serial,
+            200
+        )
+
+    return response
+    
+
+@app.route('/baked_goods', methods =['POST'])
+def baked_goods():
+    new_baked_good = BakedGood(
+        name = request.form.get('name'),
+        price = int(request.form.get('price')),
+        bakery_id = request.form.get('bakery_id')
+    )
+
+    db.session.add(new_baked_good)
+    db.session.commit()
+
+    new_baked_good_serial = new_baked_good.to_dict()
 
     response = make_response(
-        bakery_serialized,
-        200
-    )
+        new_baked_good_serial,
+        201)
+    
     return response
+
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
@@ -64,6 +101,20 @@ def most_expensive_baked_good():
         most_expensive_serialized,
         200
     )
+    return response
+
+@app.route('/baked_goods/<int:id>', methods = ['DELETE'])
+def baked_goods_by_id(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    db.session.delete(baked_good)
+    db.session.commit()
+
+    response = make_response({
+        "delete_successful": True,
+        'message': 'Baked Good Deleted'
+    },
+    200)
+
     return response
 
 if __name__ == '__main__':
